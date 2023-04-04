92df231f750cea0a0ebcfde4789c6a91cc5804a8
Bkumar48 <87478283+Bkumar48@users.noreply.github.com>
2023-04-04 18:44:51 +0530

    Add files via upload

---
+++ app/Routers/UserRoute.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {
+  createUser,
+  loginUserCtrl,
+   getallUser, 
+   getoneUser,
+   pushRoleToUser,
+   deleteaUser,
+   updateaUser,
+   logout
+} = require("../Controllers/UserController");
+const { authMiddleware} = require("../Middleware/authMiddleware");
+const router = express.Router();
+
+router.post("/register",  authMiddleware, createUser);
+router.post("/login", loginUserCtrl);
+router.get("/all-users", authMiddleware, getallUser); 
+router.get("/getUser", authMiddleware, getoneUser); 
+router.post("/pushRoleToUser", authMiddleware,pushRoleToUser);
+router.put("/updateUser", authMiddleware, updateaUser); 
+router.delete("/delUser", authMiddleware, deleteaUser);
+router.get("/logout", authMiddleware, logout);
+
+
+
+ 
+
+module.exports = router;
+  
